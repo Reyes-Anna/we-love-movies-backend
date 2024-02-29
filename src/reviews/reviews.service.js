3e80ec9be14b617ebb63c7e28590059d8955e8f8
Anna Reyes <avreyes703@outlook.com>
2024-02-28 19:23:50 -0500

    review file changes

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -33,13 +33,25 @@ function readReviewsForMovie(movie_id) {
 //gives 404 if ID is incorrect
 //response included review record and critic info set to the critic property
 function update(updatedReview) {
-    return knex("reviews as r")
-    .join("movies as m", "m.movie_id", "r.movie_id")
-    .join("critic as c", "r.critic_id", "c.critic_id")
-    .distinct("m.movie_id")
+    knex("reviews")
     .select("*")
     .where({ review_id: updatedReview.review_id})
-    .update(updatedReview, "c.*", "r.")
+    .update(updatedReview, "*")
+
+    return knex("reviews as r")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select(
+        "r.*",
+        "c.critic_id as c_critic_id",
+        "c.preferred_name",
+        "c.surname",
+        "c.organization_name",
+        "c.created_at as c_created_at",
+        "c.updated_at as c_updated_at",
+    )
+    .where({ review_id: updatedReview.review_id})
+    .first()
+    .then(addCritic)
 
 }
 
